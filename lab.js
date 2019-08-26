1436af17631b37551f5ade73866964ddfe21c67c
vnssachrslla <vanessa.chrestella@gmail.com>
2019-08-26 16:52:37 +1000

    test information

---
+++ lab.js
@@ -0,0 +1,43 @@
+let express = require('express');
+let app = express();
+let db = [];
+let bodyParser = require('body-parser');
+
+app.use(express.static("css"));
+app.use(express.static("images"));
+app.use(express.static("views"));
+app.use(bodyParser.urlencoded({
+    extended : false
+}))
+
+app.engine("html", require('ejs').renderFile);
+app.set("view engine", "html");
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+
+})
+
+app.get("/addNewTask", (req,res)=>{
+    res.sendFile(__dirname + "/views/newtask.html");
+})
+
+app.get('/listAllTask', (req,res)=>{
+    res.render("listAllTask.html", {
+        task:db
+    })
+})
+
+app.post('/newTask', (req,res)=>{
+    console.log(req.body);
+    db.push(req.body);
+    res.send("Push Success");
+    console.log(db);
+   
+})
+
+
+
+app.listen(8080, ()=>{
+    console.log("Listening on 3500");
+});
